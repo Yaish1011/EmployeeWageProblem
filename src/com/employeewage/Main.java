aa74ffa7328f30ed9debe9c213c684228c48e03b
Yaish1011 <ymakode4@gmail.com>
2024-11-25 11:39:31 +0530

    UC3: Calculate the Employee Daily Wage

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -9,14 +9,24 @@ public class Main {
         System.out.println("Welcome to Employee Wage Computation");
 
 
+        final int WAGE_PER_HOUR = 20;    // Wage per hour
+        final int FULL_DAY_HOURS = 8;   // Full day working hours
+
+
+
         Random random = new Random();
         // 0 for absent, 1 for present
         int attendance = random.nextInt(2);
 
         if (attendance == 1) {
+            int dailyWage = FULL_DAY_HOURS * WAGE_PER_HOUR;
             System.out.println("Employee is Present.");
+            System.out.println("Daily Employee Wage: $" + dailyWage);
+
         } else {
             System.out.println("Employee is Absent.");
+            System.out.println("Daily Employee Wage: $0");
+
         }
     }
 }
