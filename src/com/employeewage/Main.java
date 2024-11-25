007dbe73170f62a81ff93008e5008666d6e39f82
Yaish1011 <ymakode4@gmail.com>
2024-11-25 11:04:11 +0530

    UC1: Checks Employee is Absent or Present

--- src/com/employeewage/Main.java
+++ src/com/employeewage/Main.java
@@ -1,9 +1,22 @@
 package com.employeewage;
 
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
+
+        Random random = new Random();
+        // 0 for absent, 1 for present
+        int attendance = random.nextInt(2);
+
+        if (attendance == 1) {
+            System.out.println("Employee is Present.");
+        } else {
+            System.out.println("Employee is Absent.");
+        }
     }
 }
